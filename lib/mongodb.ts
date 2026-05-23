0f93d5d7388e8d1eedce67e54fdb980c73290562
Susu1998-cyber <susanthsankar1998@gmail.com>
2026-05-23 17:23:31 +0530

    fix: type

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,4 +1,33 @@
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+// if (!MONGODB_URI) {
+//   throw new Error("Please define MONGODB_URI");
+// }
+
+// let cached = global.mongoose;
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// export async function connectDB() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
 import mongoose from "mongoose";
+
+declare global {
+  var mongoose: { conn: typeof import("mongoose") | null; promise: Promise<typeof import("mongoose")> | null };
+}
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -16,7 +45,7 @@ export async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI as string).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -1,4 +1,37 @@
+// import nodemailer from "nodemailer";
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export async function sendNewBlogEmail(to: string, blog: any) {
+//   const blogUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${blog._id}`;
+
+//   await transporter.sendMail({
+//     from: `"Nexora Blog" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject: `🚀 New Blog Published: ${blog.title}`,
+//     html: `
+//       <h2>${blog.title}</h2>
+//       <p>${blog.description}</p>
+//       <a href="${blogUrl}" target="_blank">Read Full Article</a>
+//       <br/><br/>
+//       <small>If you no longer want these emails, you can unsubscribe anytime.</small>
+//     `,
+//   });
+// }
+
 import nodemailer from "nodemailer";
+
+type Blog = {
+  _id: string;
+  title: string;
+  description: string;
+};
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -8,7 +41,7 @@ export const transporter = nodemailer.createTransport({
   },
 });
 
-export async function sendNewBlogEmail(to: string, blog: any) {
+export async function sendNewBlogEmail(to: string, blog: Blog) {
   const blogUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${blog._id}`;
 
   await transporter.sendMail({
