b5af3f3c3f64ada0476f576738c09951a33c9d39
Susu1998-cyber <susanthsankar1998@gmail.com>
2026-05-24 19:26:23 +0530

    fix: type

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,31 +1,3 @@
-// import { connectDB } from "@/lib/mongodb";
-// import User from "@/models/User";
-// import bcrypt from "bcryptjs";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function POST(req: NextRequest) {
-//   try {
-//     const { name, email, password, role } = await req.json();
-
-//     await connectDB();
-
-//     const hashed = await bcrypt.hash(password, 10);
-
-//     const user = await User.create({
-//       name,
-//       email,
-//       password: hashed,
-//       role,
-//     });
-
-//     return NextResponse.json({ success: true, user });
-//   } catch (err) {
-//     const errorMessage =
-//       err instanceof Error ? err.message : "Internal server error";
-
-//     return NextResponse.json({ success: false, error: errorMessage });
-//   }
-// }
 import { connectDB } from "@/lib/mongodb";
 import User from "@/models/User";
 import bcrypt from "bcryptjs";
@@ -35,13 +7,14 @@ type RegisterBody = {
   name: string;
   email: string;
   password: string;
-  role?: string;
+  // role?: string;
 };
 
-export async function POST(req: NextRequest) { // ✅ fix here
+export async function POST(req: NextRequest) {
+  // ✅ fix here
   try {
     const body: RegisterBody = await req.json();
-    const { name, email, password, role } = body;
+    const { name, email, password } = body;
 
     await connectDB();
 
@@ -51,11 +24,10 @@ export async function POST(req: NextRequest) { // ✅ fix here
       name,
       email,
       password: hashed,
-      role,
+       role: "user",
     });
 
     return NextResponse.json({ success: true, user });
-
   } catch (err) {
     const errorMessage =
       err instanceof Error ? err.message : "Internal server error";
